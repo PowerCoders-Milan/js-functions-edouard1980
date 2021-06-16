ecf380a268edd3586f1e6c9f3361db415cfcef22
edouard1980 <excellenceorphe@gmail.com>
2021-06-16 12:20:36 +0200

    exo11

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -1,15 +1,18 @@
 // define a function that will check if I can vote or not
 const canIVote = age => {
-    // define a variable legalAge
 
+    // define a variable legalAge
+    const legalAge = 18
     // create if else statement that console.log() if you can vote or not
-    if (condition) {
-        
+    if (age >= legalAge) {
+        console.log('i can vote');
+
     } else {
-        
+        console.log('i can not  vote');
     }
 }
 
 // define a variable for your age
-
+let age = 19;
 // call the function and see if you can vote
+canIVote(age);
